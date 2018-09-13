30130e7b980e67602cb869bd1d1a2f88c00d55bc
Graham <telegraham@users.noreply.github.com>
2018-09-13 10:28:43 -0400

    Create cheat_sheet.rb

---
+++ cheat_sheet.rb
@@ -0,0 +1,65 @@
+dogs = [
+  {
+    name: "fido",
+    age: 4,
+    likes: ["play fetch", "bark", "piddle"]
+  },
+  {
+    name: "spot",
+    age: 10,
+    likes: ["sleep"]
+  },
+  {
+    name: "rover",
+    age: 6,
+    likes: ["chasing own tail", "rolling in dead things"]
+  }
+]
+
+
+## .each
+
+# Used for:
+# Method returns:
+# Block should return: 
+
+# sample usage
+dogs.each do |dog|
+  # do something clever here
+end
+
+
+## .map
+
+# Used for:
+# Method returns:
+# Block should return: 
+
+# sample usage
+dogs.map do |dog|
+  # do something clever here
+end
+
+
+## .select
+
+# Used for:
+# Method returns:
+# Block should return: 
+
+# sample usage
+dogs.select do |dog|
+  # do something clever here
+end
+
+
+## .find
+
+# Used for:
+# Method returns:
+# Block should return: 
+
+# sample usage
+dogs.find do |dog|
+  # do something clever here
+end
